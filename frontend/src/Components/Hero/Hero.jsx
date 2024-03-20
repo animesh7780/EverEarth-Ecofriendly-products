251b2b2d085ec92e50645ba50097e31e69586e39
animesh7780 <thakuranimesh235@gmail.com>
2024-03-20 12:23:38 +0530

    update

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Hero.css';
-import hand_icon from '../Assets/hand_icon.png';
 import arrow_icon from '../Assets/arrow.png';
 import hero_image from '../Assets/hero_image.png';
 
@@ -8,17 +7,12 @@ export const Hero = () => {
     return (
         <div className='hero'>
             <div className='hero-left'>
-                <h2>New Arrivals Only</h2>
-                <div>
-                    <div className='haro-hand-icon'>
-                        <p>new</p>
-                        <img src={hand_icon} alt='' />
-                    </div>
-                    <p>collection</p>
-                    <p>for everyone</p>
+                <div className='tagline'>
+                    <p>The future is green.</p>
+                    <p>Shop it now.</p>
                 </div>
                 <div className='hero-latest-btn'>
-                    <div>Latest Collection</div>
+                    <div>Fresh Release</div>
                     <img src={arrow_icon} alt='' />
                 </div>
             </div>
