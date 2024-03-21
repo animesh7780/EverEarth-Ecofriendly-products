72532f75683b4e2cc8786485d57cfea606920988
animesh7780 <thakuranimesh235@gmail.com>
2024-03-21 22:34:32 +0530

    homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import { ShopCategory } from './Pages/ShopCategory';
 import { Product } from './Pages/Product';
 import { Cart } from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
+import Footer from './Components/Footer/Footer';
+
 
 function App() {
   return (
@@ -25,6 +27,7 @@ function App() {
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignup />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
