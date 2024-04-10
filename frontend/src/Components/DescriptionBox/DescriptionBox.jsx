aae57b7aea71db10c2353895e4cdd82146870775
animesh7780 <thakuranimesh235@gmail.com>
2024-04-10 09:54:32 +0530

    update main

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DescriptionBox = () => {
+    return (
+        <div>DescriptionBox</div>
+    )
+}
+
+export default DescriptionBox
--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -20,7 +20,7 @@ const ProductDisplay = (props) => {
             </div>
             <div className="productdisplay-right">
                 <h1>{product.name}</h1>
-                <div className="productdisplay-right-star">
+                <div className="productdisplay-right-stars">
                     <img src={star_icon} alt="" />
                     <img src={star_icon} alt="" />
                     <img src={star_icon} alt="" />
@@ -29,7 +29,7 @@ const ProductDisplay = (props) => {
                     <p>122</p>
                 </div>
                 <div className="productdisplay-right-prices">
-                    <div className="productdisplay-right-pricr-old">${product.old_price}</div>
+                    <div className="productdisplay-right-price-old">${product.old_price}</div>
                     <div className="productdisplay-right-price-new">${product.new_price}</div>
                 </div>
                 <div className="productdisplay-right-description">
@@ -37,7 +37,7 @@ const ProductDisplay = (props) => {
                 </div>
                 <div className="productdisplay-right-size">
                     <h1>Select Size</h1>
-                    <div className="productdisplay-right-size">
+                    <div className="productdisplay-right-sizes">
                         <div>S</div>
                         <div>M</div>
                         <div>L</div>
